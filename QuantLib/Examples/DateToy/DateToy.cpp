af2d9dfedf2fdacac81017ced40c884753ff812c
Brown, Daniel J <DanielJ.Brown@tdsecurities.com>
2016-08-24 08:52:40 +0100

    Initial Commit

---
+++ QuantLib/Examples/DateToy/DateToy.cpp
@@ -0,0 +1,25 @@
+// DateToy.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <ql/time/date.hpp>
+
+using namespace QuantLib;
+
+int main()
+{
+	Date veryFirstDate(1, January, 1901);
+	Date anotherVeryFirstDate(367);
+	Date yetAnotherVeryFirstDate(veryFirstDate);
+
+	std::cout << (Date(10,December,1979)).weekday() <<"\n";
+
+	std::cout << veryFirstDate << "\n"; 
+	std::cout << anotherVeryFirstDate << "\n"; 
+	std::cout << yetAnotherVeryFirstDate << "\n";
+	std::cout << Date::minDate() << "\n"; 
+	std::cout << Date::maxDate() << "\n"; 
+	
+	return 0;
+
+}
